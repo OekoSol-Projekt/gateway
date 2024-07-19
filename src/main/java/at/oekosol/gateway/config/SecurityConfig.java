3cceb3b408c88723ab799c24df97d2c37aa6d508
Jonathan Schort <e12225164@student.tuwien.ac.at>
2024-07-20 00:15:56 +0200

    feat: make gateway only act as gateway not as user service

--- src/main/java/at/oekosol/gateway/config/SecurityConfig.java
+++ src/main/java/at/oekosol/gateway/config/SecurityConfig.java
@@ -1,19 +1,9 @@
 package at.oekosol.gateway.config;
 
-import at.oekosol.gateway.security.CustomUserDetailsService;
-import at.oekosol.gateway.security.JwtAuthenticationFilter;
-import at.oekosol.gateway.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
@@ -21,27 +11,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Autowired
-    @Qualifier("customUserDetailsService")
-    private CustomUserDetailsService customUserDetailsService;
-
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http.authorizeExchange(exchanges -> exchanges.pathMatchers("/auth/**", "/auth/login").permitAll().pathMatchers("/", "/home").permitAll().anyExchange().authenticated()).csrf().disable().httpBasic().disable().formLogin().disable().addFilterAt(new JwtAuthenticationFilter(jwtUtil, customUserDetailsService), SecurityWebFiltersOrder.AUTHENTICATION).build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public ReactiveAuthenticationManager reactiveAuthenticationManager() {
-        UserDetailsRepositoryReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(customUserDetailsService);
-        authenticationManager.setPasswordEncoder(passwordEncoder());
-        return authenticationManager;
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/users/register", "/users/login").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtSpec -> jwtSpec
+                                .jwkSetUri("http://localhost:9090/.well-known/jwks.json")
+                        )
+                );
+        return http.build();
     }
 }
--- src/main/java/at/oekosol/gateway/entity/AuthenticationRequest.java
+++
@@ -1,9 +0,0 @@
-package at.oekosol.gateway.entity;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationRequest {
-    private String username;
-    private String password;
-}
